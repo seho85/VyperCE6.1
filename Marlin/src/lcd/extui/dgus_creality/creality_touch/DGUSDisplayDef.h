84461de5e983720b6fa5e97877b983685b0497e3
Pmatsol58 <91394298+Pmatsol58@users.noreply.github.com>
2021-12-01 01:48:59 +0000

    Update DGUSDisplayDef.h
    Shorten max length of strings to fit in boxes correctly

--- Marlin/src/lcd/extui/dgus_creality/creality_touch/DGUSDisplayDef.h
+++ Marlin/src/lcd/extui/dgus_creality/creality_touch/DGUSDisplayDef.h
@@ -190,11 +190,12 @@ constexpr uint16_t VP_PID_AUTOTUNE_BED = 0x2420;
 // // Firmware version on the boot screen.
 constexpr uint16_t VP_PRINTER_BEDSIZE = 0x1074;
 constexpr uint16_t VP_PRINTER_BEDSIZE_LEN = 12;
+
 constexpr uint16_t VP_MARLIN_VERSION = 0x2222;
-constexpr uint8_t VP_MARLIN_VERSION_LEN = 32;
+constexpr uint8_t VP_MARLIN_VERSION_LEN = 30;
 
 constexpr uint16_t VP_MARLIN_WEBSITE = 0x2242;
-constexpr uint8_t VP_MARLIN_WEBSITE_LEN = 32;  
+constexpr uint8_t VP_MARLIN_WEBSITE_LEN = 30;  
 
 constexpr uint16_t VP_STANDBY_BACKLIGHT_ICON = 0x2280;
 constexpr uint16_t VP_STANDBY_BACKLIGHT_TOGGLE = 0x2282;
