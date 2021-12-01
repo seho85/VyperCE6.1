cc743260f7cac966108a9dda1a6c65179b6b0332
Pmatsol58 <91394298+Pmatsol58@users.noreply.github.com>
2021-12-01 01:29:03 +0000

    Update ui_api.cpp
    Fix call parameters to match actual function

--- Marlin/src/lcd/extui/ui_api.cpp
+++ Marlin/src/lcd/extui/ui_api.cpp
@@ -1161,7 +1161,7 @@ void MarlinUI::kill_screen(PGM_P const error, PGM_P const component) {
 
 #if HAS_BUZZER
 void MarlinUI::buzz(const long duration, const uint16_t freq) {
-  ExtUI::onPlayTone(duration, freq);
+  ExtUI::onPlayTone(freq, duration);
 }
 #endif // HAS_BUZZER
 
